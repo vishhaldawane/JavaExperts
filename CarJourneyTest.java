0ccef25637d3062fc81f52329005d2af7aa54781
vishhaldawane <45403067+vishhaldawane@users.noreply.github.com>
2021-08-07 07:33:48 +0530

    Add files via upload

--- CarJourneyTest.java
+++ CarJourneyTest.java
@@ -11,17 +11,63 @@ to be handled "before journey" and "during journey"
             KeyNotFoundException    RedSignalDishonouredInvoiceException
    DrivingLicenceNotFoundException  FlatTyreException
    InsurancePaperNotFoundException  SpeedLimitCrossedException
+
+   15 to 20 mnts to solve this
 */
+class KeyNotFoundException extends Exception {
+    KeyNotFoundException(String str) {
+        super(str);
+    }
+}
+class DrivingLicenceNotFoundException extends Exception {
+    DrivingLicenceNotFoundException(String str) {
+        super(str);
+    }
+}
+class InsurancePaperNotFoundException extends Exception {
+    InsurancePaperNotFoundException(String str) {
+        super(str);
+    }
+}
+class  RedSignalDishonouredInvoiceException extends RuntimeException {
+    RedSignalDishonouredInvoiceException(String str) {
+        super(str);
+    }
+}
+class FlatTyreException extends RuntimeException {
+    FlatTyreException(String str) {
+        super(str);
+    }
+}
+class SpeedLimitCrossedException extends RuntimeException {
+    SpeedLimitCrossedException(String str) {
+        super(str);
+    }
+}
+class RandomNumberGenerator
+{
+    public static int randomNumber(int Min, int Max)
+    {
+        return (int) (Math.random()*(Max-Min))+Min;
+    }
+}
 class Car
 {
     void startCar() {
         System.out.println("Starting the Engine...");
         shortJourney();
     }
-    private void shortJourney() {
+    private void shortJourney()
+    {
         System.out.println("Short Journey started...");
         for(int i=1;i<=5;i++) {
-            System.out.println("miles finished "+i);
+
+            int randomSpeed =RandomNumberGenerator.randomNumber(1,120);
+            System.out.println("miles finished : current speed "+randomSpeed);
+            if(randomSpeed > 80 ) {
+                SpeedLimitCrossedException slce = new SpeedLimitCrossedException("Oh!! Speed limit exceeded 80mph : currentSpeed :  "+randomSpeed);
+                throw slce;
+            }
         }
         System.out.println("Short Journey finished...");
         stopCar();
