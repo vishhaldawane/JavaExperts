77b7daa2f065890477e27b55d0eb8c56096f39da
vishhaldawane <45403067+vishhaldawane@users.noreply.github.com>
2021-08-13 07:02:08 +0530

    Add files via upload

--- GenericClassTest.java
+++ GenericClassTest.java
@@ -1,20 +1,42 @@
+/*
+
+for every primitive there is a wrapper class
+            |                   |
+            byte                Byte
+            short               Short
+            int                 Integer
+            long                Long
+            char                Character
+            float               Float
+            double              Double
+            boolean             Boolean
+
+            there are situations where only Object types
+            are allowed and not the primitive
+
+            in java there are ready made classes
+            they might need Integer and not the int
+
+ */
 public class GenericClassTest {
     public static void main(String[] args) {
-        MyPair mip = new MyPair(10,20);
+        MyPair<Integer> mip = new MyPair<Integer>(10,20);
         mip.showPair();
         mip.decoratePair();
         mip.swapPair();
         mip.showPair();
         System.out.println("-----------");
 
-        MyPair mfp = new MyPair(10.5f,20.8f);
+  //arraylist linkedlist, treeset and hashset are generic classes
+
+        MyPair<Float> mfp = new MyPair<Float>(10.5f,20.8f);
         mfp.showPair();
         mfp.decoratePair();
         mfp.swapPair();
         mfp.showPair();
         System.out.println("-----------");
 
-        MyPair msp = new MyPair("Robert","Julia");
+        MyPair<String> msp = new MyPair<String>("Robert","Julia");
         msp.showPair();
         msp.decoratePair();
         msp.swapPair();
@@ -22,7 +44,7 @@ public class GenericClassTest {
 
         System.out.println("-----------");
 
-        MyPair mdp = new MyPair(55.77,88.99);
+        MyPair<Double> mdp = new MyPair<Double>(55.77,88.99);
         mdp.showPair();
         mdp.decoratePair();
         mdp.swapPair();
@@ -33,11 +55,28 @@ public class GenericClassTest {
         Song s1 = new Song("I want it that way","Millennium","Backstreet Boys",1995);
         Song s2 = new Song("My Heart Will Go On","Titanic","Celine D",1998);
 
-        MyPair songPair = new MyPair(s1,s2);
+        //a wine glass is only for wine, not for milk or water
+        //a water glass is only for water, not for wine or milk
+        //a milk glass is only for milk, not for wine or water
+
+
+        //Plate is for the fried rice and not for milk
+        //Glass is for the milk, not for the fried rice
+
+        MyPair<Song> songPair = new MyPair<Song>(s1,s2);
         songPair.showPair();
         songPair.decoratePair();
         songPair.swapPair();
         songPair.showPair();
+        System.out.println("-------------");
+
+        MyPair<Character> emptyPair = new MyPair<Character>();
+        emptyPair.showPair();
+        emptyPair.setX('P'); //here it is a character
+        emptyPair.setY('Q'); //but now a String,
+        emptyPair.showPair();
+
+
     }
 }
 class Song {
@@ -67,12 +106,21 @@ class Song {
 // <T> is just a place holder replaced with the type you want
 
 //below class is ready for ANY DATA TYPE !!!! THATS THE POWER OF GENERIC
-class MyPair<T> //purpose is to create a pair of two numbers
+class MyPair<AnyType> //purpose is to create a pair of two numbers
 {
-    T x; //create an integer data pair here
-    T y;
-    MyPair(T a, T b) { //constructor - set it
+    AnyType x; //create an integer data pair here
+    AnyType y;
+    MyPair(AnyType a, AnyType b) { //constructor - set it
         x = a; y = b;
+    }
+    MyPair() {
+
+    }
+    void setX(AnyType at) {//basically its an java.lang.Object type
+        x = at;
+    }
+    void setY(AnyType at) {
+        y = at;
     }
     void showPair() { //show the pair
         System.out.println("x : "+x+" y : "+y);
@@ -81,7 +129,7 @@ class MyPair<T> //purpose is to create a pair of two numbers
         System.out.println("[ x ]  {"+x+"} [ y ]  {"+y+"}");
     }
     void swapPair() { //swap the pair
-        T temp = x;
+        AnyType temp = x;
         x = y;
         y = temp;
         System.out.println("swapped...my integer pair...");
